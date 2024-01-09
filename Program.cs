727d319893c04ce8d693872a632f273b9657d0e8
Arseny <arseniy.kondrashin@me.com>
2024-01-09 18:02:37 +0300

    input

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ namespace sem4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Сколько элементов будет в массиве?");
+            int len = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите строки");
+            string value = "";
+            string[] array = new string[len];
+            for (int i = 0; i < len; i++)
+            {
+                value = Console.ReadLine();
+                array[i] = value;
+            }
+            Console.WriteLine("Получившийся массив:");
+            foreach (string i in array) Console.Write("{0} ", i);
 
         }
     }
